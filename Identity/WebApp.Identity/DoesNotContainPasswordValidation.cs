02870a434b7aca2a665f5d73c25d2cb21e2e8ac9
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-07-26 21:32:27 -0300

    Configurações adicionais de segurança na aplicação.

---
+++ Identity/WebApp.Identity/DoesNotContainPasswordValidation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Identity
+{
+    public class DoesNotContainPasswordValidation<TUser> : IPasswordValidator<TUser> where TUser : class
+    {
+        public async Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user, string password)
+        {
+            var username = await manager.GetUserNameAsync(user);
+
+            if (username == password)
+                return IdentityResult.Failed(
+                    new IdentityError { Description = "Senha não pode ser igual ao nome do usuário" });
+
+            if(password.Contains("password"))
+                return IdentityResult.Failed(
+                    new IdentityError { Description = "A senha não ser Password" });
+
+            return IdentityResult.Success;
+        }
+    }
+}
