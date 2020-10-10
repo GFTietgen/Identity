1de009def50012f2d613198c8dc278f1ce8003d6
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-10-09 23:29:20 -0300

    Criação das configurações de banco de dados, JWT e configurações de segurança para o logar no sistema.

--- Identity/WebAPI.Identity/Startup.cs
+++ Identity/WebAPI.Identity/Startup.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using System.Reflection;
+using WebAPI.Domiain;
 using WebAPI.Repository;
+using System.Text;
 
 namespace WebAPI.Identity
 {
@@ -28,15 +26,49 @@ namespace WebAPI.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            var connectionString = @"Data Source=LAPTOP-HKEORKLP;Initial Catalog=API_IdentityCurso;Integrated Security=True;Persist Security Info=False;";
             var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-            
+
             services.AddDbContext<Context>(
-                options => options.UseSqlServer(connectionString,
-                sql     => sql.MigrationsAssembly(migrationAssembly)
+                options => options.UseSqlServer(Configuration.GetConnectionString("default"),
+                sql => sql.MigrationsAssembly(migrationAssembly)
                 ));
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.AllowedForNewUsers = true;
+            })
+            .AddEntityFrameworkStores<Context>()
+            .AddRoleValidator<RoleValidator<Role>>()
+            .AddRoleManager<RoleManager<Role>>()
+            .AddSignInManager<SignInManager<User>>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    });
+
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                    .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling =
+                                    Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +79,7 @@ namespace WebAPI.Identity
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
