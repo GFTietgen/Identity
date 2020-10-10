28d7fe73252ef8254cd38101a742a591f697706e
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-10-10 15:35:14 -0300

    Adicionando e configurando JWT Sistema agora suporta geração de tokens JWT quando é realizado o Login e o Registro.

---
+++ Identity/WebAPI.Identity/Helper/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Domiain;
+using WebAPI.Identity.DTO;
+
+namespace WebAPI.Identity.Helper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserLoginDTO>().ReverseMap();
+        }
+    }
+}
